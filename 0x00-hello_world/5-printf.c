475ba89c51578e050785b4d76652b33a271b0a13
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-03 22:42:17 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - puts line
+ * Description: uses puts show text
+ * Return: void
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
