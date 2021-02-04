357c526f70eedd5ad17fd2e5d3bdf932f19d2792
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-04 23:09:28 +0000

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,6 @@
 
 /**
  *  main - Positive o Negative
- *
  *  Description: Uses if and else if
  *  Return: Return string
  */
@@ -19,11 +18,11 @@ int main(void)
 	j = n % 10;
 
 	if (j > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, j);
+		printf("Last digit of %d is %d and is greater than 5\n", n, j);
 	else if (j == 0)
-		printf("Last digit of %i is %i and is 0\n", n, j);
+		printf("Last digit of %d is %d and is 0\n", n, j);
 	else if (j < 6 & j != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, j);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 
 	return (0);
 
