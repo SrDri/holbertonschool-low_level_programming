4765cb1463adf51b17fe4872e3fe773cb8df4da7
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-05 02:45:18 +0000

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Numbers
+ *
+ *  Description: Uses for
+ *  Return: void
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
