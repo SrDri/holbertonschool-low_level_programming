5be60af7df7fe670960b2a3725bdc252ea8a4eb4
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-05 00:36:56 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - Alphabetic
+ *
+ *  Description: Uses for
+ *  Return: void
+ */
+
+int main(void)
+{
+	char ch, ah;
+	for (ch = 97; ch <= 122; ch++)
+		putchar(ch);
+
+	for (ah = 65; ah <= 90; ah++)
+		putchar(ah);
+
+	putchar('\n');
+	return (0);
+}
