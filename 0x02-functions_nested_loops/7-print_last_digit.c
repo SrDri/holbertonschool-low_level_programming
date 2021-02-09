cc5525e4eddb75e6dc02bb998fee951e1ee6db3b
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-09 18:55:37 +0000

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,17 @@
+#include "holberton.h"
 
+/**
+ * print_last_digit - prints the last digit
+ * @n: int to pull last digit from
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = _abs(n);
+	last %= 10;
+	_putchar(last + '0');
+	return (last);
+}
