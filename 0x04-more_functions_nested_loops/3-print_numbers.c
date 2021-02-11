bacc9e880ce02532bf1b7f4e20e6d4ab0f54d2b0
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-11 23:56:56 +0000

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - 1 to 9
+ * main - 1 to 9
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+		_putchar (a);
+
+	_putchar(10);
+}
