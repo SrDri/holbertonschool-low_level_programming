a4f28439c59492f8b0884636bfecd5913432dd8b
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-11 19:19:31 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - prints the multiplication
+ * @a: multiplicador 1
+ * @b: multiplicador 2
+ * Return: Multiple
+*/
+
+int mul(int a, int b)
+{
+	int mp = a * b;
+	return (mp);
+}
