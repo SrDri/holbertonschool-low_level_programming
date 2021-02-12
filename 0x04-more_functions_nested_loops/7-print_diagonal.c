79fd55a51771870cf3176eee29b3f4acbff033ef
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-12 02:29:12 +0000

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Print a diagonal line
+ * @n: longitud
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, espacio;
+
+	espacio = 0;
+	if (n <= 0)
+		_putchar(10);
+	while (n > 0)
+	{
+		i = 0;
+		while (espacio != i)
+		{
+			_putchar(' ');
+			i++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		n--;
+		espacio++;
+	}
+}
