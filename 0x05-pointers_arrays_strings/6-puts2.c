8803b5bbe95736066bc811a65893361f829ef343
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-16 04:43:15 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - imprime
+ * @str: string
+ * Return: s - string
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		if (a % 2 == 0)
+			_putchar(str[a]);
+	_putchar(10);
+}
