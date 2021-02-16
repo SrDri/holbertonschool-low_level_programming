ffec1723bb490e10d84cbe759946b41c8a11fa32
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-16 17:37:57 +0000

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - imprime mitad
+ * @str: string
+ * Return: s - string
+ */
+
+void puts_half(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+		if ((n - 1) / 2 >= 2)
+			_putchar(str[n]);
+	_putchar(10);
+}
