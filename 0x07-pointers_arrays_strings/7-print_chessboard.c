0dc151d05a8d6050d0c0190c2c6edd8b3276b3ed
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-23 01:02:58 +0000

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_shessboard - char comparation
+ * @a: pointer
+ * Return: return pointer
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
