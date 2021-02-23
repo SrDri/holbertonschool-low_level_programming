24c8b5694820620ec93a21d67e070521573ae2eb
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-23 05:25:05 +0000

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,15 +15,11 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-		}
-		if (haystack[i] == needle[j] && needle[j] != '\0' && haystack[i] != '\0')
-		{
-			i++;
-			j++;
-		}
-		if (haystack[i] == 32)
-		{
-			return (haystack + i);
+			if (haystack[i] == 32)
+			{
+				return (haystack + i);
+				break;
+			}
 		}
 	}
 	return ('\0');
