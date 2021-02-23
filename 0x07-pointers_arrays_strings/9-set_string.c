596a2803e37270ca5aa0b293f3d0629b1b9db20e
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-23 03:13:10 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - setear
+ * @s: pointer
+ * @to: pointer 2
+ * Return: return pointer
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
