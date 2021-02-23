9b61d9b53046f6c00cf50c7492a406bdac941208
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-23 00:16:27 +0000

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - char comparation
+ * @haystack: pointer
+ * @needle: comparation
+ * Return: return pointer
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+		}
+			if (haystack[i] == 32)
+			{
+				i++;
+				return (haystack + i);
+			}
+	}
+	return ('\0');
+}
