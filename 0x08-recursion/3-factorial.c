b5221f269a237abd00c2391068e56f3dd765288e
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-25 00:12:23 +0000

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * factorial - factor
+ * @n: pointer
+ * Return: returns pointer
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n-1));
+	}
+}
