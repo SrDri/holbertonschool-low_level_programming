578422c8b1a8ec04d90eef2fc55cf8c43cfbc342
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-27 22:25:24 +0000

    Task 0

---
+++ 0x09-static_libraries/holberton.h
@@ -0,0 +1,25 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
+int _islower(void);
+int _isalpha(void);
+int _abs(void);
+int _isupper(void);
+int _isdigit(void);
+int _strlen(void);
+void _puts(char *s);
+char *_strcpy(void);
+int _atoi(void);
+char *_strcat(void);
+char *_strncat(void);
+char *_strncpy(void);
+int _strcmp(void);
+char *_memset(void);
+char *_memcpy(void);
+char *_strchr(void);
+unsigned int _strspn(void);
+char *_strpbrk(void);
+char *_strstr(void);
+
+#endif /* _HOLBERTON_H_ */
