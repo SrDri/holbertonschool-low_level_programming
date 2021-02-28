6643cf787eb5c6a5f5efbc0c0fc3ff06eba7e6a9
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-02-28 03:45:59 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print
+ * @argc: arguement 1
+ * @argv: argument 2
+ * Return: none
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+	else
+	{
+		argv[0] = argv[4];
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
