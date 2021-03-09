5a2c38554b0050d384d3b300cb86454ba9b02ee1
Juan Jose Carabali <juanjcarabali@gmail.com>
2021-03-09 02:54:33 +0000

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - struct dog
+ * @d: pointer to variable
+ * @name: name of a dog - string
+ * @age: age of a dog - float
+ * @owner: owner of a dog - string
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
