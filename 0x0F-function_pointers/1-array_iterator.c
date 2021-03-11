7eb54ce74cbf83542ea2cede6634d4831dab1720
SrDri <juanjcarabali@gmail.com>
2021-03-11 03:24:20 +0000

    Fix task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,10 @@
 #include "function_pointers.h"
 
 /**
-* print_name - print name
-* @name: string
-* @f: pointer
+* array_iterator - print name
+* @array: pointer array
+* @size: long
+* @action: action
 * Return: f name
 */
 
