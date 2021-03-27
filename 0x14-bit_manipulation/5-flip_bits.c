4cfbaf70136fee27291187474dbb0ad2bf444958
SrDri <juanjcarabali@gmail.com>
2021-03-27 10:36:03 +0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_bit - set
+ * @n: decimal number
+ * @index: index
+ * Return: 1 o -1 on error
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	
+}
