f8faf2608d74efc4ba1e68d22323f169302a93fc
SrDri <juanjcarabali@gmail.com>
2021-03-30 11:03:49 +0700

    Fix task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -36,9 +36,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
-
 	len = _strlen(text_content);
 
     if (text_content != NULL)
@@ -49,7 +46,7 @@ int append_text_to_file(const char *filename, char *text_content)
             return (-1);
         }
     }
-    
+
 	if (w == -1)
 		return (-1);
 
