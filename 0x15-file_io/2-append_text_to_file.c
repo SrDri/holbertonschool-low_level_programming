895e93e1b4697dc9e9a9f1987692bf84c586abd8
SrDri <juanjcarabali@gmail.com>
2021-03-30 10:54:17 +0700

    Fix task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,18 +11,18 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != 0; i++)
-	{
-	}
+    {
+        ;
+    }
 
 	return (i);
 }
 
 /**
- * append_text_to_file - ppends a text at the end of a file
- * @filename: the name of the file
- * @text_content: text to add
- *
- * Return: 1 on success, -1 on failure
+ * append_text_to_file - text add the end file
+ * @filename: name file
+ * @text_content: text
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -41,7 +41,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	len = _strlen(text_content);
 	w = write(fd, text_content, len);
-    
+
 	if (w == -1)
 		return (-1);
 
