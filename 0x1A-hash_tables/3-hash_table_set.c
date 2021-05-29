dcfe2f3e87f82b02f69cfbb1b71faca0f0e7f3c0
SrDri <juanjcarabali@gmail.com>
2021-05-29 10:46:44 +0700

    Fix betty task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
-
 	idx = key_index((const unsigned char *) key, ht->size);
 	aux_key = ht->array[idx];
 
@@ -28,18 +27,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		aux_key = aux_key->next;
 	}
-
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-
 	new->key = strdup(key);
 	if (new->key == NULL)
 	{
 		free(new);
 		return (0);
 	}
-
 	new->value = strdup(value);
 	if (new->value == NULL)
 	{
@@ -47,7 +43,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new);
 		return (0);
 	}
-
 	new->next = NULL;
 	if (ht->array[idx] != NULL)
 		new->next = ht->array[idx];
