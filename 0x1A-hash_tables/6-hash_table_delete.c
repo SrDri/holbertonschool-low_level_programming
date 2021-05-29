a8ef22229e13bc9551a33035fa5a8ada61ef3f86
SrDri <juanjcarabali@gmail.com>
2021-05-29 11:31:34 +0700

    Update task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+ * Return: void
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *actual, *aux;
+	unsigned int i = 0;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		actual = ht->array[i];
+		while (actual != NULL)
+		{
+			aux = actual->next;
+
+			free(actual->value);
+			free(actual->key);
+			free(actual);
+
+			actual = aux;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
